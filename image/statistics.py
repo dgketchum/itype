7b99bdb8bb11107cda559676a22288315deb05af
dgketchum <pdWs7kyy>
2021-02-23 09:42:13 -0700

    initial commit statistics

---
+++ image/statistics.py
@@ -0,0 +1,16 @@
+import os
+from torch import tensor
+
+""" Mean and Standard Deviation of Datasets
+
+['R', 'G', 'B', 'N', 'std_ndvi', 'mx_ndvi', 'itype'] 
+
+"""
+
+STATS = {'six_channel': (tensor([112.9420, 121.1706, 92.4031, 172.7918, 0.2031, 0.6118]),
+                         tensor([35.2243, 25.6436, 23.3244, 32.1788, 0.0503, 0.1340]))
+         }
+
+if __name__ == '__main__':
+    pass
+# ========================= EOF ====================================================================
