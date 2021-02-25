be0798d5f10e45177371410623001dc38289b11b
dgketchum <pdWs7kyy>
2021-02-24 19:38:50 -0700

    test metrics

--- learning/metrics.py
+++ learning/metrics.py
@@ -1,11 +1,12 @@
 import numpy as np
-import torch
 
+y =    np.array([0, 1, 2, 3, 2, 3, 2, 0, 1, 2, 3, 0, 2, 1, 0, 2])
+pred = np.array([0, 2, 2, 3, 2, 3, 2, 0, 2, 2, 1, 3, 2, 3, 3, 2])
 
-def get_conf_matrix(y, pred, n_class, device):
-    batch_conf = torch.tensor(np.zeros((n_class, n_class))).to(device)
+def get_conf_matrix(y, pred, n_class):
+    batch_conf = np.zeros((n_class, n_class))
     for i in range(len(y)):
-        batch_conf[y[i]][pred[i]] += 1
+        batch_conf[y[i], pred[i]] += 1
     return batch_conf
 
 
@@ -19,9 +20,9 @@ def confusion_matrix_analysis(mat):
 
     for j in range(mat.shape[0]):
         d = {}
-        tp = torch.sum(mat[j, j])
-        fp = torch.sum(mat[:, j]) - tp
-        fn = torch.sum(mat[j, :]) - tp
+        tp = np.sum(mat[j, j])
+        fp = np.sum(mat[:, j]) - tp
+        fn = np.sum(mat[j, :]) - tp
 
         d['iou'] = tp / (tp + fp + fn + epsilon)
         d['precision'] = tp / (tp + fp + epsilon)
@@ -38,7 +39,7 @@ def confusion_matrix_analysis(mat):
                'precision': TP / (TP + FP + epsilon),
                'recall': TP / (TP + FN + epsilon),
                'f1-score': 2 * TP / (2 * TP + FP + FN + epsilon),
-               'accuracy': torch.sum(torch.diag(mat)) / torch.sum(mat)}
+               'accuracy': np.sum(np.diag(mat)) / np.sum(mat)}
 
     overall = {k: v.item() for k, v in overall.items()}
 
@@ -46,5 +47,8 @@ def confusion_matrix_analysis(mat):
 
 
 if __name__ == '__main__':
-    pass
+    conf = get_conf_matrix(y, pred, n_class=4)
+    cma, overall = confusion_matrix_analysis(conf)
+    print(conf)
+    print(cma['0'])
 # =======================================================================================
